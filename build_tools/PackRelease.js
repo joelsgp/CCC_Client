f6da531fa9e736cf6d52ad754fd6e7256bdea463
jmcb <joelsgp@protonmail.com>
2022-06-23 17:48:55 +0100

    Finish fixing up PackRelease.js

--- build_tools/PackRelease.js
+++ build_tools/PackRelease.js
@@ -1,53 +1,28 @@
-const fs = require("fs");
+const { exec } = require("child_process");
 const path = require("path");
 const admzip = require("adm-zip");
 const helpers = require("../webpack/helpers");
 
-function getIgnore() {
-    let gitignore = fs.readFileSync(".gitignore", "utf-8");
-    let lines = gitignore.split("\n").filter(Boolean);
-    let ignore = new RegExp(lines.join("|"))
-    return lines
-}
 
 function packReleaseZip() {
-    let releaseDir = helpers.getReleaseDir();
-    let from = path.resolve(releaseDir);
     let releaseName = helpers.getReleaseName();
-    let zipPath = path.resolve(`./dist/${releaseName}.zip`)
-    let zip = new admzip();
+    let releasePath = "./dist/" + releaseName;
+    let from = path.resolve(releasePath);
+    let zipPath = path.resolve(`./dist/${releaseName}.zip`);
+    
     console.log("Pack " + from);
+    let zip = new admzip();
     zip.addLocalFolder(from);
     zip.writeZip( path.resolve(zipPath) );
     console.log("Release zip created! " + zipPath);
 }
 
 function packMozillaSource() {
-    let sourcesPath = path.resolve(".");
-    console.log("Pack sources " + sourcesPath)
     let releaseName = helpers.getReleaseName();
-    let zipPath = path.resolve(`./dist/${releaseName}_sources.zip`)
-    let zip = new admzip();
+    let zipPath = `./dist/${releaseName}_sources.zip`
 
-    let ignore = getIgnore();
-    for (let file of fs.readdirSync(sourcesPath)) {
-        // Hide elements
-        if (file.startsWith(".") || ignore.includes(file)) {
-            continue;
-        }
-
-        // Add to zip
-        let filePath = path.join(sourcesPath, file);
-        let stats = fs.lstatSync( filePath );
-        if (stats.isDirectory()) {
-            zip.addLocalFolder( filePath, file );
-        }
-        else {
-            zip.addLocalFile( filePath );
-        }
-    }
-
-    zip.writeZip( zipPath );
+    console.log("Pack sources from git HEAD")
+    exec(`git archive -o "${zipPath}" HEAD`);
     console.log("Source packed! " + zipPath);
 }
 
